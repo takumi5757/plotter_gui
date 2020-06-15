591d3d47ea3445270dce78e09699f506126d976e
takumi5757 <takumi5757@gmail.com>
2020-06-15 15:27:02 +0900

    コードリファクタ、16進数は自動で変換　ver1.1

--- plot_graph_on_GUI.py
+++ plot_graph_on_GUI.py
@@ -1,202 +1,192 @@
+import tkinter as tk
+import random as rd
+import time
 import pandas as pd
 import matplotlib.pyplot as plt
-import tkinter as Tk
-import statistics as st
+
+class Model():
+    def __init__(self):
+        self.width=700
+        self.height=420
 
 
-def hexadecimal_to_decimal(df, PickUpSigName):
-    """ 
-    16進数のデータを10進数に変換,0x◯◯のようなデータも扱う
-    "0x◯◯"がstr型の場合に動作
-    @params:
-        df: pd.DataFrame
-    @return:
-        df: pd.DataFrame
-    """
-    map_obj = map(lambda x: int(x, base=16), df[PickUpSigName])
-    df = pd.DataFrame(map_obj, columns=[PickUpSigName])
-    return df
+class View():
+    def __init__(self,master,model):
+        self.master = master
+        self.model = model
 
-#Create the Animation on the Graph
-def UpdateData_Graph(event):
-    #Set files name to read
-    Kaikaku_FileName = value #Result of PZ1A file name
-    Kaikaku_FileName2 = value3
-    SigName = value2
+        self.view1()
+        self.view2()
+        self.view3()
+        self.view4()
 
-    #Set parameters
-    PickUpSigName = SigName
-    TitleName = PickUpSigName
-
-    PickUpSigNameForFile = PickUpSigName.replace(".","_")
-    PickUpSigNameForFile = PickUpSigNameForFile.replace("[","_")
-    PickUpSigNameForFile = PickUpSigNameForFile.replace("]","_")
-    PickUpSigNameForFile = PickUpSigNameForFile.replace(":","_")
-    PickUpSigNameForFile = PickUpSigNameForFile.replace("/","_")
-
-    KaikakuOutputFileName = PickUpSigNameForFile + '.csv'
-    KaikakuOutputFileName2 = PickUpSigNameForFile + '2.csv'
-
-    csv_input2 = pd.read_csv(filepath_or_buffer = Kaikaku_FileName, sep=",", usecols=[PickUpSigName])
-
-    csv_input2 = pd.read_csv(filepath_or_buffer = Kaikaku_FileName2, sep=",", usecols=[PickUpSigName])
-
-    y_1 = pd.read_csv(filepath_or_buffer=KaikakuOutputFileName, sep=",")
-    y_2 = pd.read_csv(filepath_or_buffer=KaikakuOutputFileName2, sep=",")
-    x = pd.read_csv(filepath_or_buffer=Kaikaku_FileName, usecols=[0])
-
-    #暫定的な処理
-    if type(y_1[PickUpSigName][1]) is str:
-        y_1 = hexadecimal_to_decimal(y_1, PickUpSigName)
-
-    if type(y_2[PickUpSigName][1]) is str:
-        y_2 = hexadecimal_to_decimal(y_2, PickUpSigName)
-    """
-    if "0x" in y_1[PickUpSigName][1]:
-        pass
-    if "0x" in y_2[PickUpSigName][1]:
-        pass
-    """
-    plt.title(TitleName)
-    plt.xlabel('time')
-    plt.grid(True)
-    plt.plot(x, y_1)
-    plt.plot(x, y_2)
-    plt.savefig(f'png/{PickUpSigNameForFile}.png')
-    plt.show()
+        self.Static10 = tk.Label(text=u'Copyright (c) 2020 Nissan Motor Co.,Ltd. All rights reserved')
+        self.Static10.pack(anchor=tk.W)
+        self.Static10.place(x=0, y=390)
 
 
-def set_menubar(root):
+    def view1(self):
+        #labeles
+        self.Static1 = tk.Label(text=r"File Name")
+        self.Static1.pack(anchor=tk.W)
 
-    def donothing():
-        filein = Tk.Toplevel(root)
-        button = Tk.Button(filein, text="Not Supported")
-        button.pack()
+        #Entry
+        self.EditBox1 = tk.Entry(width=70)
+        self.EditBox1.insert(tk.END,  "Input the file name.")
+        self.EditBox1.pack(anchor=tk.W)
 
-    menubar = Tk.Menu(root)
-    filemenu = Tk.Menu(menubar, tearoff=0)
-    filemenu.add_command(label="Open", command=donothing)
-    filemenu.add_command(label="Save Result", command=donothing)
-    filemenu.add_command(label="Save as...", command=donothing)
-    filemenu.add_command(label="Close", command=donothing)
+        #Button
+        self.Button_InputVal1 = tk.Button(text=u'Input File Name', width=20)
+        self.Button_InputVal1.pack(anchor=tk.W)
 
-    filemenu.add_separator()
+        self.Button_Clear1 = tk.Button(text=u'Clear File Name', width=20)
+        self.Button_Clear1.pack(anchor=tk.W)
 
-    filemenu.add_command(label="Exit", command=root.quit)
-    menubar.add_cascade(label="Tool", menu=filemenu)
+    def view2(self):
+        #labeles
+        self.Static2 = tk.Label(text=r"File Name")
+        self.Static2.pack(anchor=tk.W)
 
-    helpmenu = Tk.Menu(menubar, tearoff=0)
-    helpmenu.add_command(label="About us", command=donothing)
-    helpmenu.add_command(label="manual", command=donothing)
-    helpmenu.add_cascade(label="Help", menu=helpmenu)
+        #Entry
+        self.EditBox2 = tk.Entry(width=70)
+        self.EditBox2.insert(tk.END,  "Input the file name.")
+        self.EditBox2.pack(anchor=tk.W)
 
-    root.config(menu=menubar)
+        #Button
+        self.Button_InputVal2 = tk.Button(text=u'Input File Name', width=20)
+        self.Button_InputVal2.pack(anchor=tk.W)
 
-def set_gui():
+        self.Button_Clear2 = tk.Button(text=u'Clear File Name', width=20)
+        self.Button_Clear2.pack(anchor=tk.W)
 
-    def GetEditBoxValue(event):
-        global value
-        value = EditBox.get()
+    def view3(self):
+        #Labeles
+        self.Static3 = tk.Label(text=u'Signal Name')
+        self.Static3.pack(anchor=tk.W)
+
+        self.EditBox3 = tk.Entry(width=70)
+        self.EditBox3.insert(tk.END, "Input the signal name.")
+        self.EditBox3.pack(anchor=tk.W)
+
+        #Button
+        self.Button_InputVal3 = tk.Button(text=u'Input File Name', width=20)
+        self.Button_InputVal3.pack(anchor=tk.W)
+
+        self.Button_Clear3 = tk.Button(text=u'Clear File Name', width=20)
+        self.Button_Clear3.pack(anchor=tk.W)
+
+    def view4(self):
+        #Labeles
+        self.Static_Tollgate = tk.Label(text=u'Plot Graph')
+        self.Static_Tollgate.place(x=500, y=305)
+
+        self.Button_TollMap = tk.Button(text=u'Execute', width=20)
+        self.Button_TollMap.place(x=500, y=330)
+
+
+class Controller():
+    def __init__(self,master,model,view):
+        self.master = master
+        self.model = model
+        self.view = view
+
+        self.view.Button_InputVal1["command"] = self.GetEditBoxValue1
+        self.view.Button_Clear1["command"] = self.DeleteEntryValue1
+        self.view.Button_InputVal2["command"] = self.GetEditBoxValue2
+        self.view.Button_Clear2["command"] = self.DeleteEntryValue2
+        self.view.Button_InputVal3["command"] = self.GetEditBoxValue3
+        self.view.Button_Clear3["command"] = self.DeleteEntryValue3
+        self.view.Button_TollMap["command"] = self.UpdateData_Graph
+
+    def GetEditBoxValue1(self):
+        value = self.view.EditBox1.get()
         print(value)
 
-    def GetEditBoxValue2(event):
-        global value2
-        value2 = EditBox2.get()
-        print(value2)
+    def DeleteEntryValue1(self):
+        self.view.EditBox1.delete(0, tk.END)
 
-    def DeleteEntryValue(event):
-        EditBox.delete(0, Tk.END)
+    def GetEditBoxValue2(self):
+        value = self.view.EditBox2.get()
+        print(value)
 
-    def DeleteEntryValue2(event):
-        EditBox2.delete(0, Tk.END)
+    def DeleteEntryValue2(self):
+        self.view.EditBox2.delete(0, tk.END)
 
-    def GetEditBoxValue3(event):
-        global value3
-        value3 = EditBox3.get()
-        print(value3)
+    def GetEditBoxValue3(self):
+        value = self.view.EditBox3.get()
+        print(value)
 
-    def DeleteEntryValue3(event):
-        EditBox3.delete(0, Tk.END)
+    def DeleteEntryValue3(self):
+        self.view.EditBox3.delete(0, tk.END)
 
-    #labeles
-    Static1 = Tk.Label(text=r"File Name")
-    Static1.pack(anchor=Tk.W)
+    #Create the Animation on the Graph
+    def UpdateData_Graph(self):
 
-    #Entry
-    EditBox = Tk.Entry(width=70)
-    EditBox.insert(Tk.END,  "Input the file name.")
-    EditBox.pack(anchor=Tk.W)
+        def hexadecimal_to_decimal(df, PickUpSigName):
+            """ 
+            16進数のデータを10進数に変換,0x◯◯のようなデータも扱う
+            "0x◯◯"がstr型の場合に動作
+            @params:
+                df: pd.DataFrame
+            @return:
+                df: pd.DataFrame
+            """
+            map_obj = map(lambda x: int(x, base=16), df[PickUpSigName])
+            df = pd.DataFrame(map_obj, columns=[PickUpSigName])
+            return df
 
-    #Button
-    Button_InputVal = Tk.Button(text=u'Input File Name', width=20)
-    Button_InputVal.bind("<Button-1>", GetEditBoxValue)
-    Button_InputVal.pack(anchor=Tk.W)
+        #Set files name to read
+        Kaikaku_FileName = self.view.EditBox1.get() #Result of PZ1A file name
+        Kaikaku_FileName2 = self.view.EditBox2.get()
+        SigName = self.view.EditBox3.get()
 
-    Button_Clear = Tk.Button(text=u'Clear File Name', width=20)
-    Button_Clear.bind("<Button-1>", DeleteEntryValue)
-    Button_Clear.pack(anchor=Tk.W)
+        #Set parameters
+        PickUpSigName = SigName
+        TitleName = PickUpSigName
+        
+        PickUpSigNameForFile = PickUpSigName.replace(".","_")
+        PickUpSigNameForFile = PickUpSigNameForFile.replace("[","_")
+        PickUpSigNameForFile = PickUpSigNameForFile.replace("]","_")
+        PickUpSigNameForFile = PickUpSigNameForFile.replace(":","_")
+        PickUpSigNameForFile = PickUpSigNameForFile.replace("/","_")
+        
+        y_1 = pd.read_csv(filepath_or_buffer=Kaikaku_FileName, sep=",", usecols=[PickUpSigName])
+        y_2 = pd.read_csv(filepath_or_buffer=Kaikaku_FileName2, sep=",", usecols=[PickUpSigName])
+        x = pd.read_csv(filepath_or_buffer=Kaikaku_FileName, usecols=[0])
 
-    #Entry
-    EditBox3 = Tk.Entry(width=70)
-    EditBox3.insert(Tk.END,  "Input the file name.")
-    EditBox3.pack(anchor=Tk.W)
+        #base==16 →　base==10 if data is 'str'
+        if type(y_1[PickUpSigName][1]) is str:
+            y_1 = hexadecimal_to_decimal(y_1, PickUpSigName)
 
-    #Button
-    Button_InputVal3 = Tk.Button(text=u'Input File Name', width=20)
-    Button_InputVal3.bind("<Button-1>", GetEditBoxValue3)
-    Button_InputVal3.pack(anchor=Tk.W)
+        if type(y_2[PickUpSigName][1]) is str:
+            y_2 = hexadecimal_to_decimal(y_2, PickUpSigName)
 
-    Button_Clear3 = Tk.Button(text=u'Clear File Name', width=20)
-    Button_Clear3.bind("<Button-1>", DeleteEntryValue3)
-    Button_Clear3.pack(anchor=Tk.W)
+        plt.title(TitleName)
+        plt.xlabel('time')
+        plt.grid(True)
+        plt.plot(x, y_1)
+        plt.plot(x, y_2)
+        plt.savefig(f'png/{PickUpSigNameForFile}.png')
+        plt.show()
 
-    #Labeles
-    Static2 = Tk.Label(text=u'Signal Name')
-    Static2.pack(anchor=Tk.W)
+class Application(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack()
 
-    EditBox2 = Tk.Entry(width=70)
-    EditBox2.insert(Tk.END, "Input the signal name.")
-    EditBox2.pack(anchor=Tk.W)
+        self.model = Model()
 
-    #Button
-    Button_InputVal2 = Tk.Button(text=u'Input File Name', width=20)
-    Button_InputVal2.bind("<Button-1>", GetEditBoxValue2)
-    Button_InputVal2.pack(anchor=Tk.W)
+        master.geometry(str(self.model.width)+"x"+str(self.model.height))
+        master.title("Plot_Graph Ver 1.0")
 
-    Button_Clear2 = Tk.Button(text=u'Clear File Name', width=20)
-    Button_Clear2.bind("<Button-1>", DeleteEntryValue2)
-    Button_Clear2.pack(anchor=Tk.W)
+        self.view = View(master,self.model)
 
-    #Labeles
-    Static_Tollgate = Tk.Label(text=u'Plot Graph')
-    Static_Tollgate.place(x=500, y=305)
-
-    Button_TollMap = Tk.Button(text=u'Execute', width=20)
-    Button_TollMap.bind("<Button-1>", UpdateData_Graph)
-    Button_TollMap.place(x=500, y=330)
-
-    Static10 = Tk.Label(text=u'Copyright (c) 2020 Nissan Motor Co.,Ltd. All rights reserved')
-    Static10.pack(anchor=Tk.W)
-    Static10.place(x=0, y=390)
-
+        self.controller = Controller(master,self.model,self.view)
 
 def main():
-    #Global Objects
-    global value
-    global value2
-    global value3
-
-    #Window title
-    root = Tk.Tk()
-    root.title(u"Plot_Graph Ver 1.0")
-    root.geometry("700x420")
-
-    set_gui()
-
-    set_menubar(root)
-
-    Tk.mainloop()
-
+    win = tk.Tk()
+    app = Application(master=win)
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
