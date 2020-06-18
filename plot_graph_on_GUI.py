8ac7cdeedf0bdf2bf8be0480a13f0c2e48f8bd1a
takumi5757 <takumi5757@gmail.com>
2020-06-18 18:10:48 +0900

    複数ファイル受付(プロットの動き変)、txtの受付(試してない)

--- plot_graph_on_GUI.py
+++ plot_graph_on_GUI.py
@@ -3,6 +3,7 @@ import random as rd
 import time
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 class Model():
     def __init__(self):
@@ -16,10 +17,10 @@ class View():
         self.model = model
 
         self.filename = tk.StringVar(value="Input the File Name or Path.")
-        self.filename2 = tk.StringVar(value="Input the File Name or Path.")
+        self.filename2 = tk.StringVar(value="Input the Signal Name or Text Path.")
 
         self.view1()
-        self.view2()
+        #self.view2()
         self.view3()
         self.view4()
         self.menu()
@@ -31,7 +32,7 @@ class View():
 
     def view1(self):
         #labeles
-        self.Static1 = tk.Label(text=r"File Name 1")
+        self.Static1 = tk.Label(text=r"File Name")
         self.Static1.place(x=0, y=10)
 
         #GUIからファイルを選択
@@ -43,33 +44,20 @@ class View():
         self.Button_Clear1 = tk.Button(text=u'Clear File Name', width=20)
         self.Button_Clear1.place(x=60, y=50)
 
-    def view2(self):
-        #labeles
-        self.Static2 = tk.Label(text=r"File Name 2")
-        self.Static2.place(x=0, y=110)
-
-        #Entry
-        #GUIからファイルを選択
-        self.EditBox2 = tk.Entry(text="",textvariable= self.filename2 ,width=70)
-        self.EditBox2.place(x=0, y=130)
-        self.FileDialogButton2 = tk.Button(text='open')
-        self.FileDialogButton2.place(x=0, y=150)
-
-        self.Button_Clear2 = tk.Button(text=u'Clear File Name', width=20)
-        self.Button_Clear2.place(x=60, y=150)
-
     def view3(self):
         #Labeles
         self.Static3 = tk.Label(text=u'Signal Name')
         self.Static3.place(x=0, y=230)
 
-        self.EditBox3 = tk.Entry(width=70)
-        self.EditBox3.insert(tk.END, "Input the signal name.")
+        self.EditBox3 = tk.Entry(text="", textvariable= self.filename2, width=70)
         self.EditBox3.place(x=0, y=250)
+
+        self.FileDialogButton3 = tk.Button(text='open')
+        self.FileDialogButton3.place(x=0, y=270)
 
         #Button
         self.Button_Clear3 = tk.Button(text=u'Clear column Name', width=20)
-        self.Button_Clear3.place(x=0, y=270)
+        self.Button_Clear3.place(x=60, y=270)
 
     def view4(self):
         self.Button_TollMap = tk.Button(text=u'Plot', width=20)
@@ -93,12 +81,11 @@ class Controller():
         self.view = view
 
         self.view.Button_Clear1["command"] = self.DeleteEntryValue1
-        self.view.Button_Clear2["command"] = self.DeleteEntryValue2
         self.view.Button_Clear3["command"] = self.DeleteEntryValue3
-        self.view.Button_TollMap["command"] = self.UpdateData_Graph
+        self.view.Button_TollMap["command"] = self.list_to_graph_conveter
 
         self.view.FileDialogButton1["command"] = self.openfiledialog1
-        self.view.FileDialogButton2["command"] = self.openfiledialog2
+        self.view.FileDialogButton3["command"] = self.openfiledialog2
 
         self.view.filemenu.add_command(label="Open", command=self.donothing)
         self.view.filemenu.add_command(label="Save Result", command=self.donothing)
@@ -109,11 +96,17 @@ class Controller():
         self.view.helpmenu.add_command(label="manual", command=self.donothing)
 
     def openfiledialog1(self):
-        file = tk.filedialog.askopenfilename()
-        self.view.filename.set(file)
+        fTyp = [("","*.csv")]
+        iDir = os.path.abspath(os.path.dirname(__file__))
+        file_list = tk.filedialog.askopenfilenames(filetypes = fTyp,initialdir = iDir)
+        self.file_list = list(file_list)
+        self.view.filename.set(file_list)
+
     def openfiledialog2(self):
-        file = tk.filedialog.askopenfilename()
-        self.view.filename2.set(file)
+        fTyp = [("","*.txt")]
+        iDir = os.path.abspath(os.path.dirname(__file__))
+        file_ = tk.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
+        self.view.filename2.set(file_)
 
     def donothing(self):
         self.filein = tk.Toplevel(self.master)
@@ -123,15 +116,31 @@ class Controller():
     def DeleteEntryValue1(self):
         self.view.EditBox1.delete(0, tk.END)
 
-
-    def DeleteEntryValue2(self):
-        self.view.EditBox2.delete(0, tk.END)
-
     def DeleteEntryValue3(self):
         self.view.EditBox3.delete(0, tk.END)
 
+    def list_to_graph_conveter(self):
+        #Set files name to read
+        FileName_list = self.file_list #Result of PZ1A file name
+        SigName_list = self.view.EditBox3.get() # list.txt
+
+        if str(os.path.splitext(SigName_list)[1]) == '.txt':
+            f = open(SigName_list)
+            data = f.read()
+            SigName_list = data.split('\n')
+
+        
+        #plot_num = len(SigName_list)
+        #fig,axes = plt.subplots(nrows=1,ncols=plot_num,figsize=(10,8))
+        for sig_ in SigName_list:
+            for file_ in FileName_list:
+                self.UpdateData_Graph(file_, sig_)
+
+
     #Create the Animation on the Graph
-    def UpdateData_Graph(self):
+    def UpdateData_Graph(self, FileName, SigName):
+        print("filesig")
+        print(FileName,SigName)
 
         def hexadecimal_to_decimal(df, PickUpSigName):
             """ 
@@ -141,15 +150,14 @@ class Controller():
                 df: pd.DataFrame
             @return:
                 df: pd.DataFrame
+
             """
             map_obj = map(lambda x: int(x, base=16), df[PickUpSigName])
             df = pd.DataFrame(map_obj, columns=[PickUpSigName])
             return df
 
         #Set files name to read
-        Kaikaku_FileName = self.view.EditBox1.get() #Result of PZ1A file name
-        Kaikaku_FileName2 = self.view.EditBox2.get()
-        SigName = self.view.EditBox3.get()
+        Kaikaku_FileName = FileName
 
         #Set parameters
         PickUpSigName = SigName
@@ -160,24 +168,22 @@ class Controller():
         PickUpSigNameForFile = PickUpSigNameForFile.replace("]","_")
         PickUpSigNameForFile = PickUpSigNameForFile.replace(":","_")
         PickUpSigNameForFile = PickUpSigNameForFile.replace("/","_")
-        
+
+        CSVName = os.path.split(FileName)[1].replace(".","_")
+
+        print(f"kaikaku_filename=={Kaikaku_FileName}")
         y_1 = pd.read_csv(filepath_or_buffer=Kaikaku_FileName, sep=",", usecols=[PickUpSigName])
-        y_2 = pd.read_csv(filepath_or_buffer=Kaikaku_FileName2, sep=",", usecols=[PickUpSigName])
         x = pd.read_csv(filepath_or_buffer=Kaikaku_FileName, usecols=[0])
 
         #base==16 →　base==10 if data is 'str'
         if type(y_1[PickUpSigName][1]) is str:
             y_1 = hexadecimal_to_decimal(y_1, PickUpSigName)
 
-        if type(y_2[PickUpSigName][1]) is str:
-            y_2 = hexadecimal_to_decimal(y_2, PickUpSigName)
-
         plt.title(TitleName)
         plt.xlabel('time')
         plt.grid(True)
         plt.plot(x, y_1)
-        plt.plot(x, y_2)
-        plt.savefig(f'png/{PickUpSigNameForFile}.png')
+        plt.savefig(f'png/{CSVName}_{PickUpSigNameForFile}.png')
         plt.show()
 
 class Application(tk.Frame):
